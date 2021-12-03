1caa392bd7d81b07531900101b5f8fcfccc8753a
Nidhi Kumari <nknidhi321@gmail.com>
2021-12-04 00:07:30 +0530

    Update _1_get-subsequence.java

--- recursion-in-arraylist/_1_get-subsequence.java
+++ recursion-in-arraylist/_1_get-subsequence.java
@@ -1,38 +1,7 @@
-//https://classroom.pepcoding.com/myClassroom/the-switch-program-4/recursion-in-arraylist/get-subsequence-official/ojquestion
+//https://pepcoding.com/resources/online-java-foundation/recursion-with-arraylists/get-subsequence-official/ojquestion
 
-// import java.io.*;
-// import java.util.*;
-
-// public class Main {
-
-//     public static void main(String[] args) throws Exception {
-//         Scanner sc = new Scanner(System.in);
-//         String s = sc.next();
-//         ArrayList<String> ans = gss(s);
-//         System.out.println(ans);
-//     }
-
-//     public static ArrayList<String> gss(String s) {
-//         if(s.length() == 0){
-//             ArrayList<String> list = new ArrayList<>();
-//             list.add("");
-//             return list;
-//         }
-        
-//         char ch = s.charAt(0);
-//         ArrayList<String> list = gss(s.substring(1));
-//         ArrayList<String> ans = new ArrayList<String>();
-//         for(String str : list){
-//             ans.add("" + str); 
-//         }
-//         for(String str : list){
-//             ans.add(ch + str); 
-//         }
-//         return ans;
-//     }
-
-// }
-
+//By passing idx in the recursive call
+//Efficient Approach
 
 import java.io.*;
 import java.util.*;
@@ -41,32 +10,79 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        ArrayList<String> ans = gss(s);
-        System.out.println(ans);
+        String str = sc.next();
+        System.out.println(gss(str));
     }
 
-    public static ArrayList<String> gss(String s) {
-        return gss_Util(s, 0);
+    public static ArrayList<String> gss(String str) {
+        return getsubSequence(0, str);
     }
     
-    public static ArrayList<String> gss_Util(String s, int idx) {
-        if(idx == s.length()){
+      //getType
+      public static ArrayList<String> getsubSequence(int idx, String str) {
+             if(idx == str.length()) {
+                    ArrayList<String> base = new ArrayList<>();
+                    base.add("");
+                    return base;
+              }
+
+              ArrayList<String> smallAns = getsubSequence(idx + 1, str);
+
+              //Nahi aane ki choice 
+              //As it is jo smallAns me aaya usko final answer me daal diye
+              ArrayList<String> ans = new ArrayList<>(smallAns);
+
+              //Aane ki choice
+              //Is baar saare smallAns k aage khud ko add append kar lo and add in final ans
+              char ch = str.charAt(idx);
+              for(String s : smallAns) {
+                    ans.add(ch + s);
+              }
+
+              return ans;
+        }
+
+}
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+//By passing substring in the recursive call
+//Less efficient //String ko katne ki coplexity O(N) hoti hai
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(gss(str));
+    }
+
+    //get Type
+    public static ArrayList<String> gss(String str) {
+        if(str.length() == 0) {
             ArrayList<String> list = new ArrayList<>();
             list.add("");
             return list;
         }
         
-        char ch = s.charAt(idx);
-        ArrayList<String> list = gss_Util(s, idx + 1);
-        ArrayList<String> ans = new ArrayList<String>();
-        for(String str : list){
-            ans.add("" + str); 
+        ArrayList<String> smallAns = gss(str.substring(1));
+        
+        //Nahi aane ki choice 
+	//As it is jo smallAns me aaya usko final answer me daal diye
+        ArrayList<String> ans = new ArrayList<String>(smallAns);
+        
+        //Aane ki choice
+	//Is baar saare smallAns k aage khud ko add append kar lo and add in final ans
+        char ch = str.charAt(0);
+        for(String s : smallAns) {
+            ans.add(ch + s); 
         }
-        for(String str : list){
-            ans.add(ch + str); 
-        }
+        
         return ans;
     }
 
 }
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
