3a06cb225b4c97069c2abff59ecc663545301780
Nidhi Kumari <nknidhi321@gmail.com>
2022-01-23 03:14:16 +0530

    Update _3_is-number-prime.java

--- getting-started/_3_is-number-prime.java
+++ getting-started/_3_is-number-prime.java
@@ -2,7 +2,40 @@
 
 // NOTE : 1 is prime as per their portal
 // 1 and n se toh sab number divisible hoga he, so check for (1, n) exclusively
+
+// 10^9 operation -> 1 sec
+// Acc to constraints while loop will run for 10^4 times and for loop will run for 10^9 times
+// 10^4 * 10^9 => 10^13 operations which cannot be done in 1 sec => TLE
+
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int tc = sc.nextInt();
+
+    while (tc-- > 0) {
+      int n = sc.nextInt();
+      int factors = 0;
+      for (int divisor = 2; divisor < n; divisor++) { 
+        if (n % divisor == 0) {
+          factors++;
+          break;
+        }
+      }
+      if (factors == 0) System.out.println("prime");
+      else System.out.println("not prime");
+    }
+  }
+}
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
 // More efficient : Check only until sq root of the number 
+
+// 10^9 operation -> 1 sec
+// Acc to constraints while loop will run for 10^4 times and for loop will run for sq. root of 10^9 times
 
 import java.util.*;
 
@@ -27,4 +60,4 @@ public class Main {
   }
 }
 
-//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
