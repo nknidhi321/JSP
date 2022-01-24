c5d95a44fc1dda79c39ecfacbc2c9f8e41c1877d
Nidhi Kumari <nknidhi321@gmail.com>
2022-01-24 06:17:32 +0530

    Update _7_reverse-a-number.java

--- getting-started/_7_reverse-a-number.java
+++ getting-started/_7_reverse-a-number.java
@@ -1,16 +1,15 @@
 // https://www.pepcoding.com/resources/online-java-foundation/getting-started/reverse-a-number-official/ojquestion
-
 import java.util.*;
-   
-   public class Main{
-   
-   public static void main(String[] args) {
-     // write your code here  
-     Scanner sc = new Scanner(System.in);
-     int n = sc.nextInt();
-     while(n > 0){
-         System.out.println(n % 10);
-         n = n / 10;
-     }
+
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    while (n > 0) {
+      System.out.println(n % 10);
+      n = n / 10;
     }
-   }
+  }
+}
