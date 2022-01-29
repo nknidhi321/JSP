147b2487e929cc87201ec03622140815130877cf
Nidhi Kumari <nknidhi321@gmail.com>
2022-01-30 01:33:29 +0530

    Create _1.0_duplicate-brackets.java

---
+++ stack/_1.0_duplicate-brackets.java
@@ -0,0 +1,39 @@
+// https://pepcoding.com/resources/online-java-foundation/stacks-and-queues/duplicate-brackets-official/ojquestion
+
+// If you get a closing bracket then pop until you get an opening bracket 
+// Note that the popped elements count between that opening and closing bracket should be greater than 0
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(haveDuplicateBrackets(str));
+    }
+    
+    public static boolean haveDuplicateBrackets(String str) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(ch == ')') {
+                if(stack.isEmpty()) return true;
+                else if(stack.peek() == '(') return true;
+                else {
+                    while(!stack.isEmpty() && stack.peek() != '(') stack.pop();
+                    if(!stack.isEmpty()) stack.pop();
+                }
+            }
+            else {
+                stack.push(ch);
+            }
+        }
+        return false;
+    }
+
+}
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
