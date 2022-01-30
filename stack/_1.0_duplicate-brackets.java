820b28d02e32e0a62308ff4f7e3b15a7af988a60
Nidhi Kumari <nknidhi321@gmail.com>
2022-01-30 10:59:45 +0530

    Update _1.0_duplicate-brackets.java

--- stack/_1.0_duplicate-brackets.java
+++ stack/_1.0_duplicate-brackets.java
@@ -20,11 +20,10 @@ public class Main {
         for(int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             if(ch == ')') {
-                if(stack.isEmpty()) return true;
-                else if(stack.peek() == '(') return true;
+                if(stack.peek() == '(') return true;
                 else {
                     while(!stack.isEmpty() && stack.peek() != '(') stack.pop();
-                    if(!stack.isEmpty()) stack.pop();      // pop '(' at last 
+                    if(!stack.isEmpty()) stack.pop();
                 }
             }
             else {
