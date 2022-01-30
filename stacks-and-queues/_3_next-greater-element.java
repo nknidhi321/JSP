e9a39be514b1756177812e7a0f23d6731f978e5f
Nidhi Kumari <nknidhi321@gmail.com>
2022-01-30 13:07:15 +0530

    Update _3_next-greater-element.java

--- stacks-and-queues/_3_next-greater-element.java
+++ stacks-and-queues/_3_next-greater-element.java
@@ -1,5 +1,53 @@
 // https://www.hackerrank.com/contests/second/challenges/next-greater-element/problem
 
+// Brute force
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int[] ans = nextGreaterElement(arr);
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " " + ans[i]);
+        }
+    }
+    
+    
+     //=====================================================
+     public static int[] nextGreaterElement(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(arr[j] > arr[i]) {
+                    ans[i] = arr[j];
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+    //======================================================
+    
+}
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// Using Stack
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
