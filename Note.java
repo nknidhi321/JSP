5badfca2f220e7fcbaf18ff958cd21f3d24ab736
Nidhi Kumari <nknidhi321@gmail.com>
2022-02-11 21:08:27 +0530

    Update Note.java

--- Note.java
+++ Note.java
@@ -103,4 +103,23 @@ Observation :-
 -----------
 Give one extra nextLine() in your code for that enter which you hit after 10
   
-----------------------------------------------------------------------------------------------------------------------------------------------------------  
+  
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+ 
+  
+Stack adapter using SinglyLL :-
+----------------------------
+  
+If you are using LL in the form of Stack always remember to push pop from head.
+Add new node to the head [left of the stack node chain, pointing to right]
+Because when you pop the last added element that is the head,
+Since, stack is LIFO, so when you pop an element simply move to head.next that is push pop in O(1)
+
+Suppose if you do push pop from tail then,
+When you add the new node to the last(tail) then push is O(1) && pop is O(N). Why ?
+Because, if you delete the tail your element will be popped for sure O(1) but
+now how will you move to the previous element, to maintain your tail, if it would be a singly LL ?
+Since there's no prev ptr which points to the prev node in singly LL, 
+Hence, you will again have to travel from head till the last node just to maintain your tail. So, O(N)
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
