bb580f75b4508461b67ce719a8331a8707f82ff9
Nidhi Kumari <nknidhi321@gmail.com>
2022-03-23 16:03:36 +0530

    Update and rename tiling-2*1.java to tiling-2*1_OR_ways-to-tile-a-floor.java

--- dynamic-programming/tiling-2*1_OR_ways-to-tile-a-floor.java
+++ dynamic-programming/tiling-2*1_OR_ways-to-tile-a-floor.java
@@ -20,3 +20,25 @@ public class Main {
         return dp[n] = tiling(n - 1, dp) + tiling(n - 2, dp);
     }
 }
+
+//-------------------------------------------------------------------------------
+// https://practice.geeksforgeeks.org/problems/ways-to-tile-a-floor5836/1/
+
+
+class Solution {
+
+    public static long mod = (long)1e9 + 7;
+
+    static Long numberOfWays(int n) {
+        Long[] dp = new Long[n + 1];
+        return tiling(n, dp);
+    }
+    
+    public static long tiling(int n, Long[] dp) {
+        if(n == 0 || n == 1) return dp[n] = (long)1;
+        
+        if(dp[n] != null) return dp[n];
+    
+        return dp[n] = (tiling(n - 1, dp) + tiling(n - 2, dp)) % mod;
+    }
+}
