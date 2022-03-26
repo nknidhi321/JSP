c6539eab08c5290c698b05aa887140ffd5ee6125
Nidhi Kumari <nknidhi321@gmail.com>
2022-03-26 15:46:23 +0530

    Update lowest-common-ancestor-of-a-binary-search-tree.java

--- binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,9 +1,9 @@
 // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/ 
 
-In "BST" when you find there's diversion(one lies in left && the other in right or vice-verse) of p and q node, that is your LCA
+// In "BST" when you find there's diversion(one lies in left && the other in right or vice-verse) of p and q node, that is your LCA
 
-NOTE : LCA of binary tree cannot be found using the same algo, because there nodes are not in sorted order,
-so you cannot discard your search space like you do in binary search.
+// NOTE : LCA of binary tree cannot be found using the same algo, because there nodes are not in sorted order,
+// so you cannot discard your search space like you do in binary search.
 
 ```
 class Solution {
